575428233297619e67f0999fbed07fc4a62bab85
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-05-17 19:18:04 +0200

    - Seventh update of preparing header files and program structure. - Added camera with projection and view

---
+++ libs/maths.h
@@ -0,0 +1,12 @@
+#ifndef THE_FOURTH_AGE_MATHS_H
+#define THE_FOURTH_AGE_MATHS_H
+
+typedef struct mat4 {
+
+} mat4;
+
+typedef struct vec3 {
+
+} vec3;
+
+#endif //THE_FOURTH_AGE_MATHS_H
--- libs/matrices.c
+++
@@ -1,1 +0,0 @@
-#include "matrices.h"
