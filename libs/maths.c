212766d4f52d86569bc093b8da4f3ac9062e9333
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-05-22 21:27:35 +0200

    - Started writing maths library

--- libs/maths.c
+++ libs/maths.c
@@ -1,1 +1,45 @@
 #include "maths.h"
+
+void MAT_initializeMat4Zero     (mat4 matrix) {
+    for     (int i = 0; i <= 3; i++) {
+        matrix[i]->x = 0.0f;
+        matrix[i]->y = 0.0f;
+        matrix[i]->z = 0.0f;
+        matrix[i]->w = 0.0f;
+    }
+}
+
+void MAT_initializeMat4One      (mat4 matrix) {
+    for     (int i = 0; i <= 3; i++) {
+        matrix[i]->x = 1.0f;
+        matrix[i]->y = 1.0f;
+        matrix[i]->z = 1.0f;
+        matrix[i]->w = 1.0f;
+    }
+}
+
+void MAT_initializeVec3Zero     (vec3 vector) {
+    vector.x = 0.0f;
+    vector.y = 0.0f;
+    vector.z = 0.0f;
+}
+
+void MAT_initializeVec3One      (vec3 vector) {
+    vector.x = 1.0f;
+    vector.y = 1.0f;
+    vector.z = 1.0f;
+}
+
+void MAT_initializeVec4Zero     (vec4 vector) {
+    vector.x = 0.0f;
+    vector.y = 0.0f;
+    vector.z = 0.0f;
+    vector.w = 0.0f;
+}
+
+void MAT_initializeVec4One      (vec4 vector) {
+    vector.x = 1.0f;
+    vector.y = 1.0f;
+    vector.z = 1.0f;
+    vector.w = 1.0f;
+}
--- libs/maths.h
+++ libs/maths.h
@@ -1,16 +1,31 @@
 #ifndef THE_FOURTH_AGE_MATHS_H
 #define THE_FOURTH_AGE_MATHS_H
 
-typedef struct mat4 {
-
-} mat4;
-
 typedef struct vec3 {
-
+    float x;
+    float y;
+    float z;
 } vec3;
 
 typedef struct vec4 {
-
+    float x;
+    float y;
+    float z;
+    float w;
 } vec4;
+
+typedef vec4 mat4[3][3];
+
+void MAT_initializeMat4Zero         (mat4 matrix);
+
+void MAT_initializeMat4One          (mat4 matrix);
+
+void MAT_initializeVec3Zero         (vec3 vector);
+
+void MAT_initializeVec3One          (vec3 vector);
+
+void MAT_initializeVec4Zero         (vec4 vector);
+
+void MAT_initializeVec4One          (vec4 vector);
 
 #endif //THE_FOURTH_AGE_MATHS_H
