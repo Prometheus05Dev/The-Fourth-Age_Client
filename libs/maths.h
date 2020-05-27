f3a8e738d2c79c075b23beed91f5f556946a33eb
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-05-27 09:42:18 +0200

    - Added more vector calculations

--- libs/maths.h
+++ libs/maths.h
@@ -2,6 +2,7 @@
 #define THE_FOURTH_AGE_MATHS_H
 
 #include <stdio.h>
+#include <math.h>
 
 typedef struct vec3 {
     float x;
@@ -18,28 +19,36 @@ typedef struct vec4 {
 
 typedef vec4 mat4[4];
 
-void MAT_addVec4                (vec4  *source, vec4  add);
+void  MAT_addVec4               (vec4  *source, vec4  add);
 
-void MAT_addVec3                (vec3  *source, vec3  add);
+void  MAT_addVec3               (vec3  *source, vec3  add);
 
-void MAT_subVec4                (vec4  *source, vec4  sub);
+void   MAT_subVec4              (vec4  *source, vec4  sub);
 
-void MAT_subVec3                (vec3  *source, vec3  sub);
+void   MAT_subVec3              (vec3  *source, vec3  sub);
 
-void MAT_mulVec4                (vec4  *source, float scalar);
+void   MAT_mulVec4              (vec4  *source, float scalar);
 
-void MAT_mulVec3                (vec3  *source, float scalar);
+void   MAT_mulVec3              (vec3  *source, float scalar);
 
-void MAT_crossVec4              (vec4  *source, vec4  cross);
+void   MAT_crossVec4            (vec4  *source, vec4  cross);
 
-void MAT_crossVec3              (vec3  *source, vec3  cross);
+void   MAT_crossVec3            (vec3  *source, vec3  cross);
 
-void MAT_dotVec4                (vec4  *source, vec4  cross);
+void   MAT_dotVec4              (vec4  *source, vec4  cross);
 
-void MAT_dotVec3                (vec3  *source, vec3  cross);
+void   MAT_dotVec3              (vec3  *source, vec3  cross);
 
-void MAT_identityMat4           (mat4  *source);
+void   MAT_unitVec4             (vec4  *source);
 
-void MAT_dotMat4                (mat4  *source, mat4  mul);
+void   MAT_unitVec3             (vec3  *source);
+
+double MAT_getMagnitudeVec4     (vec4  source);
+
+double MAT_getMagnitudeVec3     (vec3  source);
+
+void   MAT_identityMat4         (mat4  *source);
+
+void   MAT_dotMat4              (mat4  *source, mat4  mul);
 
 #endif //THE_FOURTH_AGE_MATHS_H
